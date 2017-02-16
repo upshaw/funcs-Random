65ef3a4739b25f0540a24690746a5fb47d287e26
upshaw <upshaw@email.arizona.edu>
2017-02-16 10:46:32 -0700

    Update basetobase.py

--- basetobase.py
+++ basetobase.py
@@ -1,31 +1,32 @@
+import re as string
 from math import modf
-from re import match
 
 def base_to_base(num,base=10,to=2):
 
   bits=[]
-  temp=[]
-  temp_fract,temp_int=modf(num)
+  temp_fract, temp_int = modf(num)
   
-  str_no_dec=match("(\d+)\.(\d*)",str(num))
-  str_length=len(str_no_dec)
+  find=string.compile("(\d+)\.(\d*)")
+  str_data = find.match(str(num))
+  str_no_dec = str_data.group(1) + str_data.group(2)
   
-  print(str_no_dec,str_length)
-  
-  idx=0
+  idx = 0 - len(str_data.group(2))
   for digit in reversed(str_no_dec):
-    temp_int+=base**idx*digit
+    temp_int+=(base**idx)*float(digit)
     idx+=1
-
+    
   while temp_num>0:
-    temp.insert(0,temp_num%to)
+    bits.insert(0,temp_num%to)
     temp_num//=to
   
   idx=50
   while idx>0"
-    
+    #TODO: decimal conversion
     if temp_fract==0:
       break
     idx-=1
     
+
+
+
 base_to_base(62.27)
