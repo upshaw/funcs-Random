63de2466ed9a9cf6c6af58a130699f9ebc6f107a
upshaw <upshaw@email.arizona.edu>
2017-02-16 23:16:47 -0700

    Update basetobase.py

--- basetobase.py
+++ basetobase.py
@@ -3,41 +3,68 @@ from math import modf
 from string import ascii_uppercase
 
 def base_to_base(num,base=10,to=2):
-  
+    
   chars=[]
   uppers=None
   
+  #add chars if needed
   max_base=max(base,to)
   if max_base>10:
-      uppers=ascii_uppercase[::max_base-10]
-  else:
-    chars=None
-  print(uppers)
+      uppers=ascii_uppercase[:max_base-10]
+      
+  #check if number is valid
+  for digit in str(num):
+    if type(digit) is str:
+      pos=uppers.find(digit)
+      try:
+        pos==-1 or pos>=base-10
+      except Exception as error:
+        print(error)
+    else:
+      try:
+        digit>=base
+      except Exception as error:
+        print(Error)
   
   bits=[]
   return_bits = lambda: "".join(bits)
   
   temp_fract, temp_int = modf(num)
   
-  find = compile("(\d+)\.(\d*)")
+  #parse to find all the numbers
+  find=None
+  if temp_fract!=0:
+    find=compile("([\u\d+]*)\.([\u\d]*)")
+  else:
+    find=compile("([\u\d+]*)")
   str_data = find.match(str(num))
-  str_no_dec = str_data.group(1) + str_data.group(2)
   
-  idx = 0 - len(str_data.group(2))
+  group2=str_data.group(2)
+  str_no_dec=str_data.group(1)
+  if type(group2) is not None:
+    str_no_dec+=group2
+    group2=len(group2)
+  else:
+    group2=0
+  
+  #convert to decimal
+  idx = 0 - group2
   for digit in reversed(str_no_dec):
     temp_int+=(base**idx)*float(digit)
     idx+=1
-    
+  
+  #convert integral part to __to__ base
   while temp_int>0:
     bits.insert(0,str(int(temp_int%to)))
     temp_int//=to
-    
+  
+  #check if fractional part needs converting
   if temp_fract==0:
     return return_bits()
   else:
     bits.append(".")
-    
   
+  #convert fractional parts
   idx=50
   while idx>0:
     temp_fract, temp_int = modf(temp_fract*to)
@@ -50,4 +77,4 @@ def base_to_base(num,base=10,to=2):
 
 
 
-print(base_to_base(62.27,12))
+print(base_to_base(62,12))
