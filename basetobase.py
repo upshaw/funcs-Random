b7c4be24f4d0cd91b4e9425506b56ed0e9566c93
upshaw <upshaw@email.arizona.edu>
2017-02-16 22:15:30 -0700

    Update basetobase.py

--- basetobase.py
+++ basetobase.py
@@ -1,12 +1,25 @@
-import re as string
+from re import compile
 from math import modf
+from string import ascii_uppercase
 
 def base_to_base(num,base=10,to=2):
-
+  
+  chars=[]
+  uppers=None
+  
+  max_base=max(base,to)
+  if max_base>10:
+      uppers=ascii_uppercase[::max_base-10]
+  else:
+    chars=None
+  print(uppers)
+  
   bits=[]
+  return_bits = lambda: "".join(bits)
+  
   temp_fract, temp_int = modf(num)
   
-  find=string.compile("(\d+)\.(\d*)")
+  find = compile("(\d+)\.(\d*)")
   str_data = find.match(str(num))
   str_no_dec = str_data.group(1) + str_data.group(2)
   
@@ -15,18 +28,26 @@ def base_to_base(num,base=10,to=2):
     temp_int+=(base**idx)*float(digit)
     idx+=1
     
-  while temp_num>0:
-    bits.insert(0,temp_num%to)
-    temp_num//=to
+  while temp_int>0:
+    bits.insert(0,str(int(temp_int%to)))
+    temp_int//=to
+    
+  if temp_fract==0:
+    return return_bits()
+  else:
+    bits.append(".")
+    
   
   idx=50
-  while idx>0"
-    #TODO: decimal conversion
+  while idx>0:
+    temp_fract, temp_int = modf(temp_fract*to)
+    bits.append(str(int(temp_int)))
     if temp_fract==0:
       break
     idx-=1
     
+  return return_bits()
 
 
 
-base_to_base(62.27)
+print(base_to_base(62.27,12))
