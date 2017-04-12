7a11f7e56288595cebb3dfc343562462d6cda2ea
upshaw <upshaw@email.arizona.edu>
2017-04-12 14:09:30 -0700

    Update ascii_bin.lua

--- ascii_bin.lua
+++ ascii_bin.lua
@@ -1,24 +1,20 @@
 local function ascii_to_bin(str)
-	local bits={}
-	str=tostring(str)
-	for i=1,#str do
+	local bits={str:byte(1,#str)}
+	for i=1,#bits do
 		local temp_bits={}
-		local bit=str:byte(i,i)
-		local int,fract=math.modf(bit)
+		local int,fract=math.modf(bits[i])
 		while int>0 do
 			local r=int%2
---			print(string.format("%01d",r))
 			table.insert(temp_bits,1,string.format("%01d",r))
 			int=(int-r)/2
 		end
-		bits[#bits+1]=string.format("%08d",table.concat(temp_bits)).." "
+		bits[i]=string.format("%08d",table.concat(temp_bits)).." "
 	end
 	return table.concat(bits)
 end
  
 local function bin_to_ascii(num)
 	local bits={}
-	num=tostring(num)
 	for k in num:gmatch(("[01]"):rep(8)) do
 		local sum=0
 		for i=0,7 do
